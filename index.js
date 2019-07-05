739541572eab463ad819ef4cbe78ba350185eff8
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-05 17:00:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function happyHolidaysTo (name){
 }
 
 function happyHolidayTo (holiday, name){
-  
+  var holiday = 'Independence Day'
+  var name = 'you'
   return `Happy ${holiday} , ${name}!`;
 }
